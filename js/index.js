e2a50b3b9692b96391c2bfd79f01cd7da97810e9
soraking <jsoraking@naver.com>
2018-09-06 18:37:08 +0900

    스타일적용 완료 (작업예정: 애니메이션 플러그인, 메뉴부분 스크립트)

--- js/index.js
+++ js/index.js
@@ -1,7 +1,14 @@
 $(document).ready(function () {
   $('.fullpage-wrap').fullpage({
     //풀페이지
-    navigation: true
+    navigation: true,
+    afterLoad: function (origin) {
+      console.log(origin);
+    },
+    //사용자가 구역을 떠나고 새로운 구역으로 이동하는 와중에 콜백
+    onLeave: function (origin, destination, direction) {
+
+    }
 
   });
 });
